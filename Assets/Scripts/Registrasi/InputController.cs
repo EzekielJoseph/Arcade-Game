c8fbefa5d68c7518b163ac22e1cce8226456c764
LAPTOP-EJ\ezekiel <ejmee1234@gmail.com>
2025-07-23 14:18:31 +0700

    BGM + Sfx

--- Assets/Scripts/Registrasi/InputController.cs
+++ Assets/Scripts/Registrasi/InputController.cs
@@ -11,12 +11,35 @@ public class InputController : MonoBehaviour
     public TMP_InputField domisiliInput;
     public TMP_InputField umurInput;
 
+    public GameObject assetPanel; 
+    public AudioClip buttonClickSfx;
+    private AudioSource audioSource;
+
+
+    void Start()
+    {
+        if (assetPanel != null)
+        {
+            assetPanel.SetActive(false); // Hide panel saat scene dimulai
+        }
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void PlayClickSfx()
+    {
+        if(audioSource != null && buttonClickSfx != null)
+        {
+            audioSource.PlayOneShot(buttonClickSfx);
+        }
+    }
+
     public void onRegister()
     {
         string nama = namaInput.text;
         string email = emailInput.text;
         string domisili = domisiliInput.text;
-        int umur = int.Parse( umurInput.text );
+        int umur = int.Parse(umurInput.text);
 
         Data userData = new Data(nama, email, domisili, umur);
 
@@ -25,5 +48,17 @@ public class InputController : MonoBehaviour
         UserDataManager.Instance.SetData(userData);
 
         SceneManager.LoadScene("Pachinko Game");
+    }
+
+    public void onAssetClick()
+    {
+        PlayClickSfx(); // Mainkan efek suara saat tombol diklik
+        assetPanel.SetActive(true); // Tampilkan panel attribution
+    }
+
+    public void onBackClick()
+    {
+        PlayClickSfx(); // Mainkan efek suara saat tombol diklik
+        assetPanel.SetActive(false); // Sembunyikan panel attribution
     }
 }
